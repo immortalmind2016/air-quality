ba1030659bd7302b81ac8dba427b284150071c39
mohamed salah <mohamedsalah.software@gmail.com>
2023-10-08 05:01:43 +0300

    chore: change test endpojnt for app e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/status')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Ok');
   });
 });
