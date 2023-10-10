5aeee3ac8bcfc263cf987403a55a541c1a2e3cd8
mohamed salah <mohamedsalah.software@gmail.com>
2023-10-10 05:32:26 +0300

    chore: remove unused service file of app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AirInformationModule } from './air-information/air-information.module';
 
 @Module({
   imports: [AirInformationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
